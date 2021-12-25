bcd104c8243450dcc7158f9b4f751cbe9d3649e0
Aramythia <54689554+Aramythia@users.noreply.github.com>
2021-12-24 20:58:58 -0500

    Start Command Reimplementation
    I can't believe this took many hours

---
+++ cogs/Error_Handler.py
@@ -0,0 +1,45 @@
+import discord
+from discord.ext import commands
+
+import sys
+import traceback
+
+from discord.ui.item import Item
+
+from Utilities import Checks
+
+class Error_Handler(commands.Cog):
+    """Bot error handler."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    # EVENTS
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Error Handling activated.")
+
+    # --- ERROR HANDLER ---
+    @commands.Cog.listener()
+    async def on_application_command_error(self, ctx, error):
+        print_traceback = True
+        
+        if isinstance(error, Checks.HasChar):
+            message = (f"You already have a character.\nFor help, read the "
+                       f"`/tutorial` or go to the `/support` server.")
+            await ctx.respond(message)
+            print_traceback = False
+
+        if isinstance(error, Checks.ExcessiveCharacterCount):
+            message = (f"Your response exceeded the character limit.\nPlease "
+                       f"keep your response under `{error.limit}` characters.")
+            await ctx.respond(message)
+            print_traceback = False
+
+        # print(error, ctx.author.id, ctx.guild.id)
+        if print_traceback:
+            traceback.print_exception(
+                error.__class__, error, error.__traceback__, file=sys.stderr)
+
+def setup(bot):
+    bot.add_cog(Error_Handler(bot))
