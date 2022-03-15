f684b7bb643a84d15a172b20873264e9689bbc35
Aramythia <54689554+Aramythia@users.noreply.github.com>
2022-03-15 18:56:03 -0400

    Add remind list command

--- cogs/Reminders.py
+++ cogs/Reminders.py
@@ -1,11 +1,13 @@
 import discord
 from discord import Option
 
-from discord.ext import commands, tasks
+from discord.ext import commands, pages, tasks
 
 import asyncio
 import time
 from datetime import timedelta
+
+from Utilities import Vars
 
 
 class Reminders(commands.Cog):
@@ -52,6 +54,38 @@ class Reminders(commands.Cog):
         self.check_for_reminders.start()
         print("Reminders is ready.")
 
+    # AUXILIARY FUNCTIONS
+    def write(self, start, reminders, player):
+        output = ''
+        iteration = 0
+        while start < len(reminders) and iteration < 5: #Loop til 5 entries or none left
+
+            time_left=timedelta(seconds=reminders[start]['endtime']-time.time())
+
+            days_left = ''
+            if time_left.days == 1:
+                days_left += '1 day, '
+            elif time_left.days > 1:
+                days_left += f'{time_left.days} days, '
+
+            fmt = "%H hours, %M minutes, %S seconds"
+            seconds = time.gmtime(time_left.seconds)
+            time_str = time.strftime(fmt, seconds)
+
+            output += (
+                f"**ID: `{reminders[start]['id']}` | "
+                f"In {days_left}{time_str}:**\n"
+                f"{reminders[start]['content']}\n")
+            
+            iteration += 1
+            start += 1
+
+        embed = discord.Embed(title=f'{player}\'s Reminders',
+            description = output,
+            color = Vars.ABLUE)
+        
+        return embed
+
     # COMMANDS
     r = discord.commands.SlashCommandGroup("remind", 
         "Set reminders for Ayesha gameplay",
@@ -88,7 +122,7 @@ class Reminders(commands.Cog):
             1 : 1, # seconds to seconds
             2 : 60, # minutes to seconds
             3 : 3600, # hours to seconds
-            4: 86400, # days to seconds
+            4 : 86400, # days to seconds
         }
         for i in range(1, len(timer)+1):
             length += int(timer[-i]) * conv[i]
@@ -99,7 +133,7 @@ class Reminders(commands.Cog):
 
         # Handle short reminders
         if length <= 60:
-            msg = await ctx.respond(f"Will remind you in {length} seconds.")
+            await ctx.respond(f"Will remind you in {length} seconds.")
             await asyncio.sleep(length)
             return await ctx.respond(f"{ctx.author.mention}: {content}")
 
@@ -118,6 +152,34 @@ class Reminders(commands.Cog):
                 """
         async with self.bot.db.acquire() as conn:
             await conn.execute(psql, starttime, endtime, ctx.author.id, content)
+
+    @r.command(name="list", guild_ids=[762118688567984151])
+    async def _list(self, ctx):
+        """Show the list of your active reminders."""
+        # Get person's active reminders
+        psql = """
+                SELECT id, starttime, endtime, user_id, content
+                FROM reminders
+                WHERE user_id = $1
+                ORDER BY endtime;
+                """
+        async with self.bot.db.acquire() as conn:
+            reminders = await conn.fetch(psql, ctx.author.id)
+
+        if len(reminders) == 0:
+            return await ctx.respond("You have no reminders!")
+
+        remind_pages = [self.write(i, reminders, ctx.author.display_name)
+            for i in range(0, len(reminders), 5)]
+
+        if len(reminders) == 1:
+            await ctx.respond(remind_pages[0])
+        else:
+            paginator = pages.Paginator(pages=remind_pages, timeout=30.0)
+            await paginator.respond(ctx.interaction)
+        
+        
+        
 
 
 def setup(bot):
