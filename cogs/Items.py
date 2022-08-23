935cfea7c97269dace6ed55a87e45a8d949ca535
Aramythia <Aramythia@users.noreply.github.com>
2022-08-23 17:00:58 -0400

    Replace inventory, armory, and wardrobe commands with full_inventory; Rename full_inventory to inventory

--- cogs/Items.py
+++ cogs/Items.py
@@ -23,37 +23,6 @@ class Items(commands.Cog):
         print("Items is ready.")
 
     # AUXILIARY FUNCTIONS
-    def create_embed(self, start, inv, got_eq):
-        embed = discord.Embed(title=f"Your Inventory", color=Vars.ABLUE)
-
-        iteration = 0
-        while start < len(inv) and iteration < 5:
-            if got_eq and start == 0:
-                embed.add_field(name=(
-                        f"{inv[start]['weapon_name']}: `{inv[start]['item_id']}` "
-                        f"[EQUIPPED]"),
-                    value=(
-                        f"**Attack:** {inv[start]['attack']}, **Crit:** "
-                        f"{inv[start]['crit']}, **Type:** "
-                        f"{inv[start]['weapontype']}, **Rarity:** "
-                        f"{inv[start]['rarity']}"
-                    ),
-                    inline=False)
-            else:
-                embed.add_field(name=(
-                        f"{inv[start]['weapon_name']}: `{inv[start]['item_id']}` "
-                        ),
-                    value=(
-                        f"**Attack:** {inv[start]['attack']}, **Crit:** "
-                        f"{inv[start]['crit']}, **Type:** "
-                        f"{inv[start]['weapontype']}, **Rarity:** "
-                        f"{inv[start]['rarity']}"
-                    ),
-                    inline=False)
-            iteration += 1
-            start += 1
-        return embed
-
     def create_item_embed(self, start, inv, size):
         embed = discord.Embed(title=f"Your Weapons", color=Vars.ABLUE)
 
@@ -76,7 +45,7 @@ class Items(commands.Cog):
             start += 1
         return embed
 
-    def create_armor_embed2(self, start, inv, size):
+    def create_armor_embed(self, start, inv, size):
         embed = discord.Embed(title="Your Armory", color=Vars.ABLUE)
 
         iteration = 0
@@ -97,7 +66,7 @@ class Items(commands.Cog):
         
         return embed
 
-    def create_accessory_embed2(self, start, inv, size):
+    def create_accessory_embed(self, start, inv, size):
         embed = discord.Embed(title="Your Wardrobe", color=Vars.ABLUE)
 
         iteration = 0
@@ -115,42 +84,12 @@ class Items(commands.Cog):
             start += 1
 
         return embed
-
-    def create_armor_embed(self, start, inv):
-        embed = discord.Embed(title="Your Armory", color=Vars.ABLUE)
-
-        iteration = 0
-        while start < len(inv) and iteration < 5:
-            mat = inv[start]['armor_type']
-            slot = inv[start]['armor_slot']
-            embed.add_field(
-                name=f"{mat} {slot}: `{inv[start]['armor_id']}`",
-                value=f"**Defense:** {Vars.ARMOR_DEFENSE[slot][mat]}%",
-                inline=False)
-            iteration += 1
-            start += 1
-        
-        return embed
-
-    def create_accessory_embed(self, start, inv):
-        embed = discord.Embed(title="Your Wardrobe", color=Vars.ABLUE)
-
-        iteration = 0
-        while start < len(inv) and iteration < 5:
-            embed.add_field(
-                name=f"{inv[start].name}: `{inv[start].id}`",
-                value=inv[start].bonus,
-                inline=False)
-            iteration += 1
-            start += 1
-
-        return embed
     
 
     # COMMANDS
-    @commands.slash_command(guild_ids=[762118688567984151])
+    @commands.slash_command()
     @commands.check(Checks.is_player)
-    async def full_inventory(self, ctx,
+    async def inventory(self, ctx,
             weapon_order : Option(str, description="Order by ATK or Crit",
                 default="item_id",
                 choices=[
@@ -267,7 +206,7 @@ class Items(commands.Cog):
             ]
 
         if armory:
-            armor_embeds = [self.create_armor_embed2(i, armory, len(armory)) 
+            armor_embeds = [self.create_armor_embed(i, armory, len(armory)) 
                 for i in range(0, len(armory), 5)]
         else:
             armor_embeds = [
@@ -275,7 +214,7 @@ class Items(commands.Cog):
             ]
 
         if wardrobe:
-            accessory_embeds = [self.create_accessory_embed2(i, wardrobe, len(wardrobe))
+            accessory_embeds = [self.create_accessory_embed(i, wardrobe, len(wardrobe))
                 for i in range(0, len(wardrobe), 5)]
         else:
             accessory_embeds = [
@@ -292,207 +231,6 @@ class Items(commands.Cog):
             show_menu=True,
             menu_placeholder="Select type of item to view")
         await paginator.respond(ctx.interaction, ephemeral=False)
-        
-
-    @commands.slash_command()
-    @commands.check(Checks.is_player)
-    async def inventory(self, ctx,
-            order : Option(str, description="Order by ATK or CRIT",
-                default="ID", 
-                choices=[
-                    OptionChoice(name="attack"), 
-                    OptionChoice(name="crit"),
-                    OptionChoice(name="ID")],
-                required=False),
-            rarity : Option(str, description="Get only a specific rarity",
-                choices=[OptionChoice(name=r) for r in Vars.RARITIES.keys()],
-                required=False),
-            weapontype : Option(str, 
-                description="Get only a specific weapon type",
-                choices=[OptionChoice(name=t) for t in Vars.WEAPON_TYPES],
-                required=False)):
-        """View your inventory."""
-        await ctx.defer()
-
-        async with self.bot.db.acquire() as conn:
-            # Get equipped item to put at top of list
-            psql1 = """
-                    WITH thing AS (
-                        SELECT equipped_item
-                        FROM players
-                        WHERE user_id = $1
-                    )
-                    SELECT items.item_id, items.weapontype, items.user_id, 
-                        items.attack, items.crit, items.weapon_name, 
-                        items.rarity
-                    FROM items
-                    INNER JOIN thing ON items.item_id = thing.equipped_item;
-                    """
-            psql2 = """
-                    SELECT item_id, weapontype, user_id, 
-                        attack, crit, weapon_name, rarity
-                    FROM items
-                    WHERE user_id = $1
-                    """
-
-            if rarity is not None and weapontype is not None:
-                psql2 += f" AND rarity = $2 AND weapontype = $3 "
-                if order == "ID":
-                    psql2 += "ORDER BY item_id;"
-                else:
-                    psql2 += f"ORDER BY {order} DESC;"
-                inv = await conn.fetch(psql2, ctx.author.id, rarity, weapontype)
-            elif rarity is not None and weapontype is None:
-                psql2 += f" AND rarity = $2 "
-                if order == "ID":
-                    psql2 += "ORDER BY item_id;"
-                else:
-                    psql2 += f"ORDER BY {order} DESC;"
-                inv = await conn.fetch(psql2, ctx.author.id, rarity)
-            elif rarity is None and weapontype is not None:
-                psql2 += f"AND weapontype = $2"
-                if order == "ID":
-                    psql2 += "ORDER BY item_id;"
-                else:
-                    psql2 += f"ORDER BY {order} DESC;"
-                inv = await conn.fetch(psql2, ctx.author.id, weapontype)
-            else:
-                if order == "ID":
-                    psql2 += "ORDER BY item_id;"
-                else:
-                    psql2 += f"ORDER BY {order} DESC;"
-                inv = await conn.fetch(psql2, ctx.author.id)
-
-            equip = await conn.fetchrow(psql1, ctx.author.id)
-            if equip is not None:
-                got_eq = True
-            else:
-                got_eq = False
-
-        inventory = [] # Allows me to put equip and rest of items in one thing
-        for record in inv:
-            inventory.append(record)
-
-        if len(inventory) == 0: # Account for equipped item as 1
-            return await ctx.respond("Your inventory is empty!")
-        else: # Create a bunch of embeds and paginate
-            if got_eq:
-                inventory.insert(0, equip)
-            # The create_embed function writes embeds; 5 per page
-            embeds = [self.create_embed(i, inventory, got_eq) 
-                for i in range(0, len(inventory), 5)]
-
-            paginator = pages.Paginator(pages=embeds, timeout=30)
-            await paginator.respond(ctx.interaction)
-
-    @commands.slash_command()
-    @commands.check(Checks.is_player)
-    async def armory(self, ctx,
-        slot : Option(str,
-            description="Get only a specific armor slot",
-            required=False,
-            choices=[OptionChoice(s) for s in Vars.ARMOR_DEFENSE]),
-        material : Option(str,
-            description="Get only a specific armor material",
-            required=False,
-            choices=[OptionChoice(m) for m in Vars.ARMOR_DEFENSE["Boots"]])):
-        """Armories held both weapons and armor, but here only armor :P"""
-        await ctx.defer()
-        async with self.bot.db.acquire() as conn:
-            psql = f"""
-                    SELECT armor_id, armor_type, armor_slot
-                    FROM armor
-                    WHERE user_id = $1 
-                    """
-
-            if slot is not None and material is not None:
-                psql += """ 
-                        AND armor_type = $2 AND armor_slot = $3
-                        ORDER BY armor_id;
-                        """
-                armory = await conn.fetch(psql, ctx.author.id, material, slot)
-            elif slot is not None and material is None:
-                psql += """
-                        AND armor_slot = $2
-                        ORDER BY armor_id;
-                        """
-                armory = await conn.fetch(psql, ctx.author.id, slot)
-            elif slot is None and material is not None:
-                psql += """
-                        AND armor_type = $2
-                        ORDER BY armor_id;
-                        """
-                armory = await conn.fetch(psql, ctx.author.id, material)
-            else:
-                psql += " ORDER BY armor_id;"
-                armory = await conn.fetch(psql, ctx.author.id)
-
-        if len(armory) == 0:
-            return await ctx.respond("Your armory is empty!")
-
-        embeds = [self.create_armor_embed(i, armory) 
-            for i in range(0, len(armory), 5)]
-        if len(embeds) == 1:
-            await ctx.respond(embed=embeds[0])
-        else:
-            paginator = pages.Paginator(pages=embeds, timeout=30)
-            await paginator.respond(ctx.interaction)
-
-    @commands.slash_command()
-    @commands.check(Checks.is_player)
-    async def wardrobe(self, ctx, 
-            prefix : Option(str,
-                description="Sort for a specific effect",
-                required=False,
-                choices=[OptionChoice(p) for p in Vars.ACCESSORY_BONUS]),
-            material : Option(str,
-                description="Sort for a specific core material",
-                required=False,
-                choices=[OptionChoice(m) for m in Vars.ACCESSORY_BONUS['Lucky']]
-            )):
-        """Your wardrobe contains all your accessories. View them here."""
-        async with self.bot.db.acquire() as conn:
-            psql = """
-                    SELECT accessory_id
-                    FROM accessories
-                    WHERE user_id = $1 
-                    """
-            if prefix is not None and material is not None:
-                psql += """
-                        AND prefix = $2 AND accessory_type = $3
-                        ORDER BY accessory_id;
-                        """
-                inv = await conn.fetch(psql, ctx.author.id, prefix, material)
-            elif prefix is None and material is not None:
-                psql += """
-                        AND accessory_type = $2
-                        ORDER BY accessory_id;
-                        """
-                inv = await conn.fetch(psql, ctx.author.id, material)
-            elif prefix is not None and material is None:
-                psql += """
-                        AND prefix = $2
-                        ORDER BY accessory_id;
-                        """
-                inv = await conn.fetch(psql, ctx.author.id, prefix)
-            else:
-                psql += " ORDER BY accessory_id;"
-                inv = await conn.fetch(psql, ctx.author.id)
-
-            if len(inv) == 0:
-                return await ctx.respond("Your wardrobe is empty!")
-
-            inv = [await ItemObject.get_accessory_by_id(
-                        conn, record['accessory_id'])
-                    for record in inv] # Turn them into objects (for the name)
-
-        embeds = [self.create_accessory_embed(i, inv)
-            for i in range(0, len(inv), 5)]
-        if len(embeds) == 1:
-            await ctx.respond(embed=embeds[0])
-        else:
-            paginator = pages.Paginator(pages=embeds, timeout=30)
-            await paginator.respond(ctx.interaction)
 
     @commands.slash_command()
     @commands.check(Checks.is_player)
