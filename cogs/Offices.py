826f310e69b81dbcd522e8547ac6ce1ab6db3ea6
Aramythia <drixel2@gmail.com>
2023-03-01 15:22:44 +0000

    Remove print statements

--- cogs/Offices.py
+++ cogs/Offices.py
@@ -62,12 +62,10 @@ class Offices(commands.Cog):
                         VALUES ((SELECT user_id FROM gold_leader), 'Comptroller')
                         RETURNING officeholder;
                         """
-                print("Fetching...")
                 new_mayor_id = await conn.fetchval(psql1)
                 new_comp_id = await conn.fetchval(psql2)
                 new_mayor = await self.bot.fetch_user(new_mayor_id)
                 new_comp = await self.bot.fetch_user(new_comp_id)
-                print(new_mayor_id, new_comp_id)
 
                 await self.bot.announcement_channel.send(
                     f"Congratulations to our new mayor {new_mayor.mention} and "
