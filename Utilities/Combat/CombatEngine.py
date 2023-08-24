a135f39ac253db8f350c398e6703d1c2f199107a
Aramythia <drixel2@gmail.com>
2023-08-24 03:52:52 +0000

    Attempt at writing CombatTurn; add Modifier class to store sources of damage, healing, etc

--- Utilities/Combat/CombatEngine.py
+++ Utilities/Combat/CombatEngine.py
@@ -1,7 +1,7 @@
 import discord
 
 import random
-from typing import Tuple, Optional
+from typing import List, Tuple, Optional
 
 from Utilities import PlayerObject, Vars
 
@@ -10,10 +10,37 @@ from Utilities.CombatObject import ActionChoice, InvalidMove, ACTION_COMBOS
 from Utilities.Combat.Action import Action
 from Utilities.Combat.Belligerent import Belligerent
 
+class Modifier:
+    def __init__(self, source, magnitude, multiplier) -> None:
+        self.source = source
+        self.magnitude = magnitude
+        self.multiplier = multiplier
+
+class DamageSource(Modifier):
+    def __init__(self, source, magnitude, multiplier) -> None:
+        super().__init__(source, magnitude, multiplier)
+    # TODO: Get proper data structure that can compress sources of the same type
 
 class CombatTurn:
-    def __init__(self):
-        pass
+    def __init__(
+            self, 
+            actor: Belligerent, 
+            target: Belligerent, 
+            action: Action, turn: int
+    ):
+        self.actor = actor
+        self.target = target
+        self.turn = turn
+
+        self.attacks = []  # Apply all sources of possible attacks e.g. attack action, acolyte effects, etc
+        self.heals = []
+        self.damages: List[DamageSource] = []  # Apply all sources of possible damage at turn start e.g. poison
+
+    def apply(self):  # When all things are calculated, run something like this to get stat changes
+        damage_taken = 0
+        for source in self.damages:
+            damage_taken += source.magnitude
+        self._damage_taken = damage_taken
 
 
 class CombatEngine:
@@ -30,6 +57,7 @@ class CombatEngine:
         self.turn = 0
         self.victor: Belligerent = None
         self.actor: Belligerent = None
+        self.target: Belligerent = None
 
         # Select first turn
         self.set_next_actor()
@@ -55,31 +83,33 @@ class CombatEngine:
     def process_turn(self, action: Action) -> CombatTurn:
         """Process a new turn"""
         self.turn += 1
-        damage, damage_multiplier = 0, 0
-        heal, heal_multiplier = 0, 0
-        is_crit = False
+        actor = self.actor
+        target = self.target
+        result = CombatTurn(actor, target, action, self.turn)
 
         # Process status effects
+        for status in self.actor.status:
+            status.on_turn(result)
 
         # Create raw damage count
         if action not in Action:
             raise InvalidMove
 
-        match action:
-            case Action.ATTACK:
-                damage = random.randint(
-                    self.actor.attack * 0.75, self.actor.attack * 1.25
-                )
-                damage_multiplier += 1
-            case Action.BLOCK:
-                pass  # Replace with BRACE - add DEF boost status effect
-            case Action.PARRY:
-                pass  # Idk maybe make a special attack
-            case Action.HEAL:
-                heal = self.actor.max_hp * .2
-                heal_multiplier += 1
-            case Action.BIDE:
-                self.actor.attack *= 1.05
+        # match action:
+        #     case Action.ATTACK:
+        #         damage = random.randint(
+        #             self.actor.attack * 0.75, self.actor.attack * 1.25
+        #         )
+        #         damage_multiplier += 1
+        #     case Action.BLOCK:
+        #         pass  # Replace with BRACE - add DEF boost status effect
+        #     case Action.PARRY:
+        #         pass  # Idk maybe make a special attack
+        #     case Action.HEAL:
+        #         heal = self.actor.max_hp * .2
+        #         heal_multiplier += 1
+        #     case Action.BIDE:
+        #         self.actor.attack *= 1.05
 
         # Determine critical strikes
 
@@ -102,7 +132,7 @@ class CombatEngine:
         while min(self.player1, self.player2).cooldown > 0:
             self.player1.cooldown -= self.player1.speed
             self.player2.cooldown -= self.player2.speed
-        self.actor = min(self.player1, self.player2)
+        self.actor, self.target, *other = sorted(self.player1, self.player2)
         self.actor.cooldown = 1000
 
 
